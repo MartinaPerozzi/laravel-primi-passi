0ab4fc4a89d20f0bae84e8db0fdc2cab3a2d6a63
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-31 18:29:26 +0200

    excercise Hello World

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,6 +1,13 @@
+    
+    
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Laravel Hello World</a>
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('images/Laravel_logo.png')}}" alt="Logo Laravel" width="40">
+                
+                {{env('APP_NAME')}}
+            
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
